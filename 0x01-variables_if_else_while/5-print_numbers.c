ed951c258a19fb93822b4229e6ad5c2df1af61c0
FORMAT1 <olayiwolafm@gmail.com>
2022-09-09 15:04:55 -0700

    Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
-
 /**
-* main - Prints the numbers since 0 to 9
+* main - print if the number is postive, zero, or negative
 *
-* Return: Always (Success)
+* Description: using the main function
+* this program prints "Programming is positive, zero, or negative
+* Return: 0
 */
 int main(void)
 {
-        char n;
-        for (n = '0'; n <= '9'; n++)
-        {
-                putchar(n);
-        }
-        putchar('\n');
-        return (0);
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+	putchar(ch);
+}
+putchar('\n');
+return (0);
 }
