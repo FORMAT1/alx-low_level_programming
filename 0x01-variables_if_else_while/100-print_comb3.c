5aebee399ca68ddf3974275f1874f125e8754ea5
FORMAT1 <olayiwolafm@gmail.com>
2022-09-09 16:09:32 -0700

    gorssa

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - print single digit numbers starting from 0
- *
- * Return: returns zero at the end
- */
+* main - print single digit numbers starting from 0
+*
+* Return: returns zero at the end
+*/
 
 int main(void)
 {
         int i, j;
         for (i = 0; i <= 9; i++)
         {
-                for (j = 1; j <= 9; j++)
+
+		for (j = 1; j <= 9; j++)
                 {
                         if (j > i)
                         {
