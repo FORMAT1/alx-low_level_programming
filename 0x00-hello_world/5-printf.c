976ec51e12a351267970b76a12e8dd079e122fc3
FORMAT1 <olayiwolafm@gmail.com>
2022-09-12 05:59:22 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -9,6 +9,5 @@
 */                                                                                                                                      
 int main(void)                                                                                                                          
 {                                                                                                                                       
-       	ptintf("with proper grammar, but the outcome is a piece of art, \n";                                                            
-       	return (0);                                                                                                                     
+       	ptintf("with proper grammar, but the outcome is a piece of art, \n");                                                            	return (0);                                                         
 }
