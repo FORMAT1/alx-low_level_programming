d5e9b8a7f108557d2aad8fa7ff28a5cff379763b
FORMAT1 <olayiwolafm@gmail.com>
2022-09-21 10:44:41 -0700

    concatenates two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,16 +1,17 @@
 #include "main.h"
 
 /**
-*_strcat - concatenates the string pointed to by @src to
-*thee end of thee string pointed to by @dest
-*@dest: String thaat will be appended 
-*@src: String to be concatenated upon
-*
-*Return: returns pointer to @dest
-*/
+ *_strcat - concatenates the string pointed to by @src to
+ *thee end of thee string pointed to by @dest
+ *@dest: String thaat will be appended 
+ *@src: String to be concatenated upon
+ *
+ *Return: returns pointer to @dest
+ */
 
 char *_strcat(char *dest, char *src)
 {
+
 	int index = 0, dest_len = 0;
 
 	while (dest[index++])
