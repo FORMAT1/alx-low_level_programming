3cd235b381909270e8d3ad034065383c528659d6
FORMAT1 <olayiwolafm@gmail.com>
2022-09-22 14:07:24 -0700

    print buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,15 +1,13 @@
 #include "main.h"
+
 #include <stdio.h>
 
 /**
- * print_line - prints a s bytes of a buffer
- * @c: buffer to print
- * @s: bytes of buffer to print
- * @l: line of buffer to print
- *
- * Return: void
- */
-
+* print_line - prints a s bytes of a buffer
+* @c: buffer to print
+* @s: bytes of buffer to print
+* @l: line of buffer to print
+*/
 void print_line(char *c, int s, int l)
 {
 	int j, k;
@@ -30,4 +28,30 @@ void print_line(char *c, int s, int l)
 		else
 			putchar('.');
 	}
+}
+
+/**
+* print_buffer - prints a buffer
+* @b: buffer to print
+* @size: size of buffer
+*/
+void print_buffer(char *b, int size)
+{
+	int i;
+
+	for (i = 0; i <= (size - 1) / 10 && size; i++)
+	{
+		printf("%08x: ", i * 10);
+		if (i < size / 10)
+		{
+			print_line(b, 9, i);
+		}
+		else
+		{
+			print_line(b, size % 10 - 1, i);
+		}
+		putchar('\n');
+	}
+	if (size == 0)
+		putchar('\n');
 }
