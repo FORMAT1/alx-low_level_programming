c5aeba392b475484713ebaf3f727bf69abc40677
FORMAT1 <olayiwolafm@gmail.com>
2022-09-22 16:18:05 -0700

    print number

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -11,16 +11,17 @@ void print_number(int n)
 {
 	unsigned int n1;
 
-	ni = n;
-
 	if (n < 0)
 	{
-		_putchar('-');
 		n1 = -n;
+		_putchar('-');
 	}
-		if (n1 / 10 != 0)
+	else
 	{
-		print_number(n1 / 10);
+		n1 = n;
 	}
+	if (n1 / 10)
+		print_number(n1 / 10);
+	
 	_putchar((n1 % 10) + '0');
 }
