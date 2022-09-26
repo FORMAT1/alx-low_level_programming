c4c8e90e8e2f1eacfce79c2d14bfffb2ff742e58
FORMAT1 <olayiwolafm@gmail.com>
2022-09-26 14:53:13 -0700

    0. memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,6 +4,7 @@
  * _memset - fills a memory block with a constant byte
  * @s: address to memory block
  * @b: char to be used
+ * @n: number of bytes to be used
  *
  * Return: pointer to the memory block
  */
@@ -15,5 +16,5 @@ char *_memset(char *s, char b, unsigned int n)
 		s[n - 1] = b;
 		n--;
 	}
-	return (s)
+	return (s);
 }
